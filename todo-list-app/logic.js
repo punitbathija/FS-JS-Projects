96ec11af4eec53b6056481a1f71ea7cacdd3ccef
Punit Bathija <punit644@gmail.com>
2022-10-04 12:46:14 +0530

    Sixth Assignment Completed

--- todo-list-app/logic.js
+++ todo-list-app/logic.js
@@ -1,25 +1,22 @@
-let board = document.getElementById("board");
-let add = document.getElementById("add");
-let list = document.getElementById("list");
-let form = document.createElement("div");
+let input = document.querySelector("#form");
+let add = document.querySelector("#add");
+let list = document.querySelector("#list");
 
 function addTask() {
-  form.classList.remove("hidden");
-  form.innerHTML = `<h4>Please Enter Your Task</h4>
-                       <input id="input"></input>
-                       <button onclick="populate()">Add</button>`;
-  board.append(form);
-}
+  list.innerHTML += `<div class = "task">
+                      <span class = "taskname">
+                         ${input.value}
+                        </span>
+                        <button class = "delete">
+                          X
+                        </button>
+                      </div>`;
 
-function populate() {
-  form.classList.add("hidden");
-  let input = document.querySelector("#input");
-  let li = document.createElement("li");
-  li.textContent = `${input.value}`;
-  list.append(li);
-  let del = document.createElement("button");
-  del.innerText = "x";
-  list.append(del);
+  let tasks = document.querySelectorAll(".delete");
+  for (let i = 0; i < tasks.length; i++) {
+    tasks[i].onclick = function () {
+      this.parentNode.remove();
+    };
+  }
 }
-
 add.addEventListener("click", addTask);
